77a9c7bacc19c7e492a95ba94ba249e599444d2f
Emil <79602784+svidt@users.noreply.github.com>
2023-05-30 22:45:25 +0100

    adjustments

--- Module 2 - Recipe List App/Recipe List App/ContentView.swift
+++ Module 2 - Recipe List App/Recipe List App/ContentView.swift
@@ -22,16 +22,81 @@ struct Card {
     
 }
 
+class Person {
+    var name: String = "Emil"
+    
+    func introduceMyself() {
+        print("Hi, my name is \(name)")
+    }
+}
+
+class Chef: Person {
+    override func introduceMyself() {
+        print("Hi, my name is \(name)")
+        print("I'm a Chef!")
+    }
+}
+
+class Poet: Person {
+    override func introduceMyself() {
+        print("Hi, my name is \(name)")
+        print("I'm a Poet!")
+    }
+}
+class Astronaut: Person {
+    override func introduceMyself() {
+        print("Hi, my name is \(name)")
+        print("I'm a Astronaut!")
+    }
+}
+
 struct ContentView: View {
     
     @State private var randomCard = ""
     @State private var historyOfCards = [String]()
     
+    @State private var arrayDemo = ["Element 1", "Element 2", "Element 3"]
+    
     var body: some View {
         
+        var num = 0
+        var classInstance = Person()
         
         NavigationView {
             VStack {
+                Button("While - Repeat loop") {
+                    repeat {
+                        classInstance.name = "Paul"
+                        classInstance.introduceMyself()
+                        print(num)
+                        num += 1
+                    } while (num < 10)
+                }
+                
+//                List(arrayDemo, id: \.self) { item in
+//                    NavigationLink(
+//                        destination: Text("Hello there"),
+//                        label: {
+//                            Text(item)
+//                        })
+//                }
+                Button("Click me!") {
+                    
+                    let num = Int.random(in: 1...10)
+                    
+                    if num != 7 {
+                        arrayDemo.append(String(num))
+                        
+                    } else {
+                        print("7")
+                    }
+                }
+                Button("Clear me!") {
+                    arrayDemo.removeAll()
+                }
+                
+                Spacer()
+                
                 Text(randomCard)
                 List(historyOfCards, id:\.self) {
                     Text($0)
@@ -44,12 +109,15 @@ struct ContentView: View {
                         historyOfCards.insert(randomCard, at: 0)
                     }
                     Button("Button 2") {
-                        print(historyOfCards)
+                        for demo in historyOfCards {
+                            print(demo)
+                        }
+                        historyOfCards.removeAll()
                     }
                 }
+                .padding()
             }
-            .padding()
-            
+            .navigationTitle("My List")
         }
     }
 }

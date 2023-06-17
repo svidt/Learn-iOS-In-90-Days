640e795cb4f0d91cffaafec83626963e78e03b6d
Emil <79602784+svidt@users.noreply.github.com>
2023-06-17 22:45:36 +0100

    New Project

---
+++ Module 3 - The Restaurant App/Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Svidt on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

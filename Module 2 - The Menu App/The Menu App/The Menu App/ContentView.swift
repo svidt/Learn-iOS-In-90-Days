cbf8dc14b49e9753a0df1f156991580297b66920
Emil <79602784+svidt@users.noreply.github.com>
2023-06-17 22:29:31 +0100

    Module 1

--- Module 2 - The Menu App/The Menu App/The Menu App/ContentView.swift
+++ Module 2 - The Menu App/The Menu App/The Menu App/ContentView.swift
@@ -16,25 +16,7 @@ struct ContentView: View {
         
         List(menuItems) { item in
             
-            HStack {
-                Image(item.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 50)
-                    .cornerRadius(10)
-                
-                Text(item.name)
-                    .bold()
-                
-                Spacer()
-                
-                Text("$" + item.price)
-            }
-            .listRowSeparator(.hidden)
-            .listRowBackground(
-                Color(.brown)
-                    .opacity(0.1)
-            )
+            MenuListRow(item: item)
         }
         .listStyle(.plain)
         .onAppear(perform: {

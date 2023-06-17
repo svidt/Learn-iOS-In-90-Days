abc8ff71d318b303f4dccd883460942b2bdc85a3
Emil <79602784+svidt@users.noreply.github.com>
2023-06-17 22:49:08 +0100

    added files

---
+++ Module 3 - The Restaurant App/README.md
@@ -0,0 +1,6 @@
+# The Restaurant App
+Module 3, part of a bigger application
+
+<p align="center">
+<img src="Screenshot.png" width="350">
+</p>
---
+++ Module 3 - The Restaurant App/Restaurant/Restaurant/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  Restaurant
+//
+//  Created by Svidt on 17/06/2023.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}

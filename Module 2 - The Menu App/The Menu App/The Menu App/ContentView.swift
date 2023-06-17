388e7200a7bec5f2f4b1d1bcad435821cef5723c
Emil <79602784+svidt@users.noreply.github.com>
2023-06-17 22:20:06 +0100

    updated

--- Module 2 - The Menu App/The Menu App/The Menu App/ContentView.swift
+++ Module 2 - The Menu App/The Menu App/The Menu App/ContentView.swift
@@ -9,18 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var menuItems:[MenuItem] = [MenuItem(name: "Onigiri", price: "1.99", imageName: "onigiri"),
-                                MenuItem(name: "Meguro Sushi", price: "5.99", imageName: "meguro-sushi"),
-                                MenuItem(name: "Tako Sushi", price: "4.99", imageName: "tako-sushi"),
-                                MenuItem(name: "Avocado Maki", price: "2.99", imageName: "avocado-maki"),
-                                MenuItem(name: "Tobiko Spicy Maki", price: "4.99", imageName: "tobiko-spicy-maki"),
-                                MenuItem(name: "Salmon Sushi", price: "4.99", imageName: "salmon-sushi"),
-                                MenuItem(name: "Hamachi Sushi", price: "6.99", imageName: "hamachi-sushi"),
-                                MenuItem(name: "Kani Sushi", price: "3.99", imageName: "kani-sushi"),
-                                MenuItem(name: "Tamago Sushi", price: "3.99", imageName: "tamago-sushi"),
-                                MenuItem(name: "California Roll", price: "3.99", imageName: "california-roll"),
-                                MenuItem(name: "Shrimp Sushi", price: "3.99", imageName: "shrimp-sushi"),
-                                MenuItem(name: "Ikura Sushi", price: "5.99", imageName: "ikura-sushi")]
+    @State var menuItems:[MenuItem] = [MenuItem]()
+    var dataService = DataService()
     
     var body: some View {
         
@@ -47,6 +37,9 @@ struct ContentView: View {
             )
         }
         .listStyle(.plain)
+        .onAppear(perform: {
+            menuItems = dataService.getData()
+        })
         
         
     }
